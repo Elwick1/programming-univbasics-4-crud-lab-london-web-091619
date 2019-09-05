0bead955731fde2e5c943fae7412adfbbb0f0039
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-09-05 15:18:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,37 +1,37 @@
-def create_an_empty_array
-  
-end
+def create_an_empty_array 
+  []
+  end
 
-def create_an_array
-  
-end
+def create_an_array 
+  ["Michael", "Kasia", "Tiff", "Zoe"]
+  end
 
 def add_element_to_end_of_array(array, element)
-  
+  (array << "arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+ add_element_to_start_of_array(array, wow.unshift)
 end
 
 def remove_element_from_end_of_array(array)
-  
+ # remove_element_from_end_of_array (create_an_array, "Ciaran".pop)
 end
 
 def remove_element_from_start_of_array(array)
-  
+ # remove_element_from_start_of_array (create_an_array.shift)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+ # retrieve_element_from_index(create_an_array, [2])
 end
 
 def retrieve_first_element_from_array(array)
-  
+ # retrieve_first_element_from_array(create_an_array [0])
 end
 
 def retrieve_last_element_from_array(array)
-  
+ # retrieve_last_element_from_array(create_an_array)
 end
 
 def update_element_from_index(array, index_number, element)
